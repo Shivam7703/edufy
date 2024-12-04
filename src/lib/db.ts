d51d6991443511e5e68479649632dde6c32cde01
shivam7703 <shivamg7703@gmail.com>
2024-12-04 17:24:15 +0530

    db

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,10 +2,10 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: "sql12.freesqldatabase.com",
+  user: "sql12749662",
+  password: "ei7VrRyd1n",
+  database: "sql12749662",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
