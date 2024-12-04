732f4a1a3028af2f82086ed1c95494f8b86ceff8
shivam7703 <shivamg7703@gmail.com>
2024-12-04 16:45:19 +0530

    home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         <div className="flex justify-center space-x-4">
           <Link 
             href="/addSchool" 
-            className="px-6 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition"
+            className="px-6 py-3 bg-blue-800 text-white rounded-lg hover:bg-blue-600 transition"
           >
             Add New School
           </Link>
